24b1242918542d869fdcfb6475aebaa85e2266d6
abdalla rahmah <abdallarahmah.dev@gmail.com>
2024-08-05 18:29:54 +0400

    remove gulp-cssnano package from the project
    - remove gulp-cssnano from package.json.
    - remove gulp-cssnano from gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
-const { src, dest, watch, series } = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const cssnano = require("gulp-cssnano");
-const sourcemaps = require("gulp-sourcemaps");
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
 
 function html() {
-  return src("./src/*.html").pipe(dest("./dist/"));
+    return src('./src/*.html').pipe(dest('./dist/'))
 }
 
-exports.html = html;
+exports.html = html
 
 function styles() {
-  return src("./src/styles/*.scss", { sourcemaps: true })
-    .pipe(sass())
-    .pipe(cssnano())
-    .pipe(sourcemaps.write("."))
-    .pipe(dest("./dist/styles/", { sourcemaps: "." }));
+    return src('./src/styles/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(dest('./dist/styles/'))
 }
 
-exports.styles = styles;
+exports.styles = styles
 
 function js() {
-  return src("./src/scripts/*.js").pipe(dest("./dist/scripts/"));
+    return src('./src/scripts/*.js').pipe(dest('./dist/scripts/'))
 }
 
-exports.js = js;
+exports.js = js
 
 function assets() {
-  return src("./src/images/*").pipe(dest("./dist/images/"));
+    return src('./src/images/*').pipe(dest('./dist/images/'))
 }
 
-exports.assets = assets;
+exports.assets = assets
 
 function watchTask() {
-  watch("./src/*.html").on("change", html);
-  watch("./src/styles/**/*.scss").on("change", styles);
-  watch("./src/scripts/*.js").on("change", js);
+    watch('./src/*.html', html)
+    watch('./src/styles/**/*.scss', styles)
+    watch('./src/scripts/*.js', js)
+    watch('./src/images/*', assets)
 }
 
-exports.watchTask = watchTask;
+exports.watchTask = watchTask
 
-exports.default = series(html, styles, js, assets, watchTask);
+exports.default = series(html, styles, js, assets, watchTask)
